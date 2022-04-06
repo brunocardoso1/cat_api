af3c2eade7a3feccad5db87a20b68bcd99d7f95a
Bruno Cardoso <bruno.alvescardoso@hotmail.com>
2022-04-06 20:59:40 -0300

    give me a cat

---
+++ lib/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color color1 = Color.fromRGBO(0, 177, 210, 1);
+Color color2 = Color.fromRGBO(255, 234, 22, 1);
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'colors.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
 
@@ -11,7 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   Future<List> pegarFotos() async {
     var url = Uri.parse('https://api.thecatapi.com/v1/images/search');
     var response = await http.get(url);
@@ -23,23 +24,34 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  Future<List> pegaNome() async {
+    var url = Uri.parse('https://api.namefake.com');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(utf8.decode(response.bodyBytes));
+    } else {
+      throw Exception('Erro ao carregar dados da Api');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: color2,
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: (){},
-        ),
-        title: Text('Meu gato miau'),
-      ),
+          backgroundColor: color1, title: Center(child: Text("The cutest cats you'll see today! maybe...", style: TextStyle(
+        fontSize: 17,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic
+      ),))),
       body: FutureBuilder<List>(
           future: pegarFotos(),
           builder: (context, snapshot) {
             if (snapshot.hasError) {
               return Center(
-                child: Text("Erro ao carregar fotos"),);
+                child: Text("Erro ao carregar fotos"),
+              );
             }
             if (snapshot.hasData) {
               return ListView.builder(
@@ -47,16 +59,37 @@ class _HomePageState extends State<HomePage> {
                 itemBuilder: (context, index) {
                   return Column(
                     children: [
-                      Image.network(snapshot.data[index]['url'])
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          width: 400,
+                          height: 400,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(1000),
+                              image: DecorationImage(
+                                image:
+                                    NetworkImage(snapshot.data[index]['url']),
+                                fit: BoxFit.cover,
+                              )),
+                        ),
+                      ),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: color1,
+                          ),
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          child: Text("Give me a new cat!")),
                     ],
                   );
-                },);
+                },
+              );
             }
             return Center(
               child: CircularProgressIndicator(),
             );
-          }
-      ),
+          }),
     );
   }
 }
